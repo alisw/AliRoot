689ddbf7d16c387e057791cd6e228450311bcbb7
mfloris <michele.floris@cern.ch>
2014-09-03 10:23:08 +0200

    AliAODEvent::GetHeader now return AliVHeader
    This required patching the base classes in a few places and moving
    some of the NanoAOD to STEER/AOD and checking which actual header is
    contained in the AODEvent class in a few places.  This commit does not
    compile (analysis tasks which use AliAODEvent::GetHeader still need to
    be patched)

--- STEER/AOD/AliNanoAODHeader.h
+++ STEER/AOD/AliNanoAODHeader.h
@@ -42,6 +42,32 @@ public:
   virtual Double_t GetSigma2DiamondY()             const    {AliError("Not Implemented"); return 0;};
   virtual Double_t GetSigma2DiamondZ()             const    {AliError("Not Implemented"); return 0;};
 
+  virtual void     SetOrbitNumber(UInt_t /* nOr */)           {AliError("Not Implemented"); };
+  virtual void     SetPeriodNumber(UInt_t /* nPer */)         {AliError("Not Implemented"); };
+  virtual void     SetBunchCrossNumber(UShort_t /* nBx */)    {AliError("Not Implemented"); };
+  virtual void     SetEventType(UInt_t /* evttype */)         {AliError("Not Implemented"); };
+  virtual void     SetTriggerMask(ULong64_t /* trigMsk */)    {AliError("Not Implemented"); };
+  virtual void     SetTriggerCluster(UChar_t /* trigClus */)  {AliError("Not Implemented"); };
+  virtual void     SetFiredTriggerClasses(TString /* trig */) {AliError("Not Implemented"); };
+  virtual TString  GetFiredTriggerClasses() const             {AliError("Not Implemented"); return "";};
+  virtual Double_t GetZDCN1Energy()         const             {AliError("Not Implemented"); return 0;};
+  virtual Double_t GetZDCP1Energy()         const             {AliError("Not Implemented"); return 0;};
+  virtual Double_t GetZDCN2Energy()         const             {AliError("Not Implemented"); return 0;};
+  virtual Double_t GetZDCP2Energy()         const             {AliError("Not Implemented"); return 0;};
+
+  virtual Double_t GetZDCEMEnergy(Int_t /* i */) const            {AliError("Not Implemented"); return 0;};
+  virtual Int_t    GetNumberOfESDTracks()  const            {AliError("Not Implemented"); return 0;};
+  virtual UInt_t   GetNumberOfITSClusters(Int_t /* ilay */) const {AliError("Not Implemented"); return 0;};
+  virtual Float_t  GetT0spread(Int_t /* i */)               const {AliError("Not Implemented"); return 0;};
+  // FIXME: THIS IS UGLY!!!!
+  // FIXME: use dynamic cast in AliAODEVent?
+  virtual AliCentrality* GetCentralityP()  const {AliError("Not Implemented"); return 0;};
+  virtual AliEventplane* GetEventplaneP()  const {AliError("Not Implemented"); return 0;};
+  virtual const Float_t* GetVZEROEqFactors() const {AliError("Not Implemented"); return 0;};
+  virtual Float_t        GetVZEROEqFactors(Int_t /* i */) const {AliError("Not Implemented"); return 0;};
+  virtual void           SetVZEROEqFactors(const Float_t* /*factors*/) {AliError("Not Implemented"); } 
+
+  virtual UInt_t GetOfflineTrigger()  {AliError("Not Implemented"); return 0;};
 
 
   virtual void Print(Option_t* /*option = ""*/) const  {Printf("I'm a special header!");}
