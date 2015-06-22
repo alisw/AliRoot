alisw/AliRoot

commit 0e846935b77687ecb62a0a5647792b6cec395a77

CMake: improved parsing of fastjet-config params
Fixes ALPHY-20

cmake/FindFASTJET.cmake
@@ -14,9 +14,11 @@
 # **************************************************************************
 
 # AliRoot Build System Module to find and configure FASTJET
-# Author: Marco van Leeuwen 
+# Authors: Marco van Leeuwen, Dario Berzano
 # FastJet installation is pointed during cmake configuration -DFASTJET
+#
 # Variables set during Find:
+#
 # - FASTJET_CONFIG - fastjet-config location
 # - FASTJET_VERSION
 # - FASTJET_VERSION_MAJOR
@@ -26,11 +28,12 @@
 # - FASTJET_LIBS_DIR - fastjet libraries location
 # - FASTJET_DEFINITIONS - fastjet definition flags
 # - FASTJET_CXXFLAGS - fastjet compilation flags
-# - FASTJET_LIBS - fastjet libraries - array
+# - FASTJET_LIBS - fastjet libraries (list)
 
 set(FASTJET_FOUND FALSE)
 
 if(FASTJET)
+
     # Check for fastjet-config script
     find_program(FASTJET_CONFIG NAMES fastjet-config PATHS ${FASTJET}/bin NO_DEFAULT_PATH)
     if(NOT FASTJET_CONFIG)
@@ -42,14 +45,14 @@ if(FASTJET)
     find_path(FASTJETHH include/fastjet/PseudoJet.hh PATHS ${FASTJET})
 
     if (FASTJETHH-NOTFOUND)
-        message(FATAL_ERROR "Header file fastjet/PseudoJet.hh not found in ${FASTJET}/include. Please check your FASTJET installation")
+        message(FATAL_ERROR "Header file fastjet/PseudoJet.hh not found in ${FASTJET}/include. Please check your FastJet installation")
     endif(FASTJETHH-NOTFOUND)
     mark_as_advanced(FASTJETHH)
 
     # FastJet version
-    execute_process(COMMAND ${FASTJET_CONFIG} --version OUTPUT_VARIABLE FASTJET_VERSION ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process(COMMAND ${FASTJET_CONFIG} --version OUTPUT_VARIABLE FASTJET_VERSION ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(error)
-        message(FATAL_ERROR "Error retrieving FastJet version : ${error}")
+        message(FATAL_ERROR "Error retrieving FastJet version: ${error}")
     endif(error)
 
     # Extract major, minor, and patch versions from
@@ -62,11 +65,11 @@ if(FASTJET)
     endif()
     
     if(FASTJET_VERSION_MINOR GREATER 0)
-        message(FATAL_ERROR "FastJet ${FASTJET_VERSION_MAJOR}.${FASTJET_VERSION_MINOR}.${FASTJET_VERSION_PATCH} version is not suported.  Please install a version >= 3.0.*")
+        message(FATAL_ERROR "FastJet ${FASTJET_VERSION_MAJOR}.${FASTJET_VERSION_MINOR}.${FASTJET_VERSION_PATCH} version is not suported. Please install a version >= 3.0.*")
     endif()
 
     # Extracting compilation flags
-    execute_process(COMMAND ${FASTJET_CONFIG} --cxxflags OUTPUT_VARIABLE FASTJET_CXXFLAGS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process(COMMAND ${FASTJET_CONFIG} --cxxflags OUTPUT_VARIABLE FASTJET_CXXFLAGS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(error)
         message(FATAL_ERROR "Error retrieving FastJet compilation flags : ${error}")
     endif(error)
@@ -74,45 +77,44 @@ if(FASTJET)
     # Extracting the include path from the CXX flags
     set(FASTJET_INCLUDE_DIR)
     if(FASTJET_CXXFLAGS)
-	string(STRIP ${FASTJET_CXXFLAGS} FASTJET_CXXFLAGS)
-	string(REGEX MATCHALL  "[-][I]([^ ])+" incfolders ${FASTJET_CXXFLAGS})
-	
-	foreach(incfolder ${incfolders})
-	    string(REPLACE "-I" "" incfolder ${incfolder})
-	    set(FASTJET_INCLUDE_DIR ${incfolder} ${FASTJET_INCLUDE_DIR})
-	endforeach()
+        string(REGEX MATCHALL "(^| )-I[^ ]+" incfolders ${FASTJET_CXXFLAGS})
+        foreach(incfolder ${incfolders})
+            string(STRIP ${incfolder} incfolder)
+            string(SUBSTRING ${incfolder} 2 -1 incfolder)
+            set(FASTJET_INCLUDE_DIR ${incfolder})
+            break()
+        endforeach()
     endif(FASTJET_CXXFLAGS)
 
     # Extracting libraries and linking options
-    execute_process(COMMAND ${FASTJET_CONFIG} --libs OUTPUT_VARIABLE FASTJET_CONFIGLIBS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process(COMMAND ${FASTJET_CONFIG} --libs OUTPUT_VARIABLE FASTJET_CONFIGLIBS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(error)
         message(FATAL_ERROR "Error retrieving FastJet libs : ${error}")
     endif(error)
     
-    # Extracting the list of needed libraries during linking and the linking folders
+    # Extracting the list of needed libraries during linking and the linking directory
     set(FASTJET_LIBS)
     set(FASTJET_LIBS_DIR)
     if(FASTJET_CONFIGLIBS)
-	string(STRIP ${FASTJET_CONFIGLIBS} FASTJET_CONFIGLIBS)
-    	
-	# Extracting the list of libraries needed during linking
-	# -l strings
-	string(REGEX MATCHALL "[-][l]([^ ])+" fjlibs "${FASTJET_CONFIGLIBS}")
-    
-	foreach(flib ${fjlibs})
-    	    string(REPLACE "-l" "" flib "${flib}")
-    	    set(FASTJET_LIBS ${FASTJET_LIBS} ${flib})
-	endforeach()
 
-	# Extracting the list of libraries folders
-	# -L strings
-	string(REGEX MATCHALL "[-][L]([^ ])+" fjlibdirs "${FASTJET_CONFIGLIBS}")
-	
-	foreach(fjlibdir ${fjlibdirs})
-	    string(REPLACE "-L" "" fjlibdir ${fjlibdir})
-	    set(FASTJET_LIBS_DIR ${fjlibdir} ${FASTJET_LIBS_DIR})
-	endforeach()
-    endif()
+        # Extract libraries needed at link time (-l): the output is a LIST
+        string(REGEX MATCHALL "(^| )-l[^ ]+" fjlibs ${FASTJET_CONFIGLIBS})
+        foreach(fjlib ${fjlibs})
+            string(STRIP ${fjlib} fjlib)
+            string(SUBSTRING ${fjlib} 2 -1 fjlib)
+            list(APPEND FASTJET_LIBS ${fjlib})
+        endforeach()
+
+        # Extract library path: only one STRING as output
+        string(REGEX MATCHALL "(^| )-L[^ ]+" fjlibdirs ${FASTJET_CONFIGLIBS})
+        foreach(fjlibdir ${fjlibdirs})
+            string(STRIP ${fjlibdir} fjlibdir)
+            string(SUBSTRING ${fjlibdir} 2 -1 fjlibdir)
+            set(FASTJET_LIBS_DIR ${fjlibdir})
+            break()
+        endforeach()
+
+    endif(FASTJET_CONFIGLIBS)
 
     set(FASTJET_FOUND TRUE)
     set(FASTJET_DEFINITIONS "-DHAVE_FASTJET")

commit 9d009af20681d78662275521acbb59c07f5b4d57

CMake: all libpaths in FastJet config
Fixes a problem with external CGAL accidentally introduced in
0e846935b77687ecb62a0a5647792b6cec395a77 (sorry)

cmake/FindFASTJET.cmake
@@ -71,25 +71,24 @@ if(FASTJET)
     # Extracting compilation flags
     execute_process(COMMAND ${FASTJET_CONFIG} --cxxflags OUTPUT_VARIABLE FASTJET_CXXFLAGS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(error)
-        message(FATAL_ERROR "Error retrieving FastJet compilation flags : ${error}")
+        message(FATAL_ERROR "Error retrieving FastJet compilation flags: ${error}")
     endif(error)
     
-    # Extracting the include path from the CXX flags
+    # Extracting the include path(s?) from the CXX flags (LIST)
     set(FASTJET_INCLUDE_DIR)
     if(FASTJET_CXXFLAGS)
         string(REGEX MATCHALL "(^| )-I[^ ]+" incfolders ${FASTJET_CXXFLAGS})
         foreach(incfolder ${incfolders})
             string(STRIP ${incfolder} incfolder)
             string(SUBSTRING ${incfolder} 2 -1 incfolder)
-            set(FASTJET_INCLUDE_DIR ${incfolder})
-            break()
+            list(APPEND FASTJET_INCLUDE_DIR ${incfolder})
         endforeach()
     endif(FASTJET_CXXFLAGS)
 
     # Extracting libraries and linking options
     execute_process(COMMAND ${FASTJET_CONFIG} --libs OUTPUT_VARIABLE FASTJET_CONFIGLIBS ERROR_VARIABLE error OUTPUT_STRIP_TRAILING_WHITESPACE)
     if(error)
-        message(FATAL_ERROR "Error retrieving FastJet libs : ${error}")
+        message(FATAL_ERROR "Error retrieving FastJet libs: ${error}")
     endif(error)
     
     # Extracting the list of needed libraries during linking and the linking directory
@@ -97,7 +96,7 @@ if(FASTJET)
     set(FASTJET_LIBS_DIR)
     if(FASTJET_CONFIGLIBS)
 
-        # Extract libraries needed at link time (-l): the output is a LIST
+        # Extract libraries needed at link time (-l) (LIST)
         string(REGEX MATCHALL "(^| )-l[^ ]+" fjlibs ${FASTJET_CONFIGLIBS})
         foreach(fjlib ${fjlibs})
             string(STRIP ${fjlib} fjlib)
@@ -105,13 +104,12 @@ if(FASTJET)
             list(APPEND FASTJET_LIBS ${fjlib})
         endforeach()
 
-        # Extract library path: only one STRING as output
+        # Extract library paths used by FastJet to find needed libs: (LIST)
         string(REGEX MATCHALL "(^| )-L[^ ]+" fjlibdirs ${FASTJET_CONFIGLIBS})
         foreach(fjlibdir ${fjlibdirs})
             string(STRIP ${fjlibdir} fjlibdir)
             string(SUBSTRING ${fjlibdir} 2 -1 fjlibdir)
-            set(FASTJET_LIBS_DIR ${fjlibdir})
-            break()
+            list(APPEND FASTJET_LIBS_DIR ${fjlibdir})
         endforeach()
 
     endif(FASTJET_CONFIGLIBS)