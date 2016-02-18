alisw/AliRoot

commit a0d4cd9ea847470958ea43859794c8c0960e9a90

ATO-290 -  Standartize metadata usage for the trending tree visualization (AxisTitle, Legend, Color MarkerStyle  ...))

STAT/TStatToolkit.cxx
@@ -977,6 +977,34 @@ TGraphErrors * TStatToolkit::MakeGraphErrors(TTree * tree, const char * expr, co
   
 }
 
+THashList*  TStatToolkit::AddMetadata(TTree* tree, const char *varTagName,const char *varTagValue){
+  //
+  // Add metadata infromation as user info to the tree - see https://alice.its.cern.ch/jira/browse/ATO-290
+  // TTree metdata are used for the Drawing methods in the folling drawing functions
+  /*
+    Supported metadata:
+    - <varName>.AxisTitle
+    - <varName>.Legend
+    - <varname>.Color
+    - <varname>.MarkerStyle
+    This metadata than can be used by the TStatToolkit
+    - TStatToolkit::MakeGraphSparse
+    - TStatToolkit::MakeGraphErrors
+   */
+  // 
+  if (!tree) return kFALSE;
+  THashList * metaData = (THashList*) tree->GetUserInfo()->FindObject("metaTable");
+  if (!metaData == 0){  
+    metaData=new THashList;
+    metaData->SetName("metaTable");
+    tree->GetUserInfo()->AddLast(metaData);
+  } 
+  if (varTagName!=NULL && varTagValue!=NULL){
+    metaData->AddLast(new TNamed(varTagName,varTagValue));
+  }
+  return metaData;
+}
+
 
 TGraph * TStatToolkit::MakeGraphSparse(TTree * tree, const char * expr, const char * cut, Int_t mstyle, Int_t mcolor, Float_t msize, Float_t offset){
   //
@@ -1099,6 +1127,7 @@ TGraph * TStatToolkit::MakeGraphSparse(TTree * tree, const char * expr, const ch
 //
 // functions used for the trending
 //
+
 
 Int_t  TStatToolkit::MakeStatAlias(TTree * tree, const char * expr, const char * cut, const char * alias) 
 {

STAT/TStatToolkit.h
@@ -47,7 +47,7 @@
 #include <float.h>
 //#include "TGraph2D.h"
 //#include "TGraph.h"
-
+class THashList;
 
 namespace TStatToolkit
 {
@@ -83,6 +83,7 @@ namespace TStatToolkit
   //
   // Graph tools
   //
+  THashList *AddMetadata(TTree*, const char *vartagName,const char *varTagValue);
   TGraph * MakeGraphSparse(TTree * tree, const char * expr="Entry", const char * cut="1",  Int_t mstyle=25, Int_t mcolor=1, Float_t msize=-1, Float_t offset=0.0);
   TGraphErrors * MakeGraphErrors(TTree * tree, const char * expr="Entry", const char * cut="1",  Int_t mstyle=25, Int_t mcolor=1, Float_t msize=-1, Float_t offset=0.0);
 

commit f8d034b718c7368d27013065172df271c8b36858

Fix build error
Was introduced in a0d4cd9ea847470958ea43859794c8c0960e9a90

STAT/TStatToolkit.cxx
@@ -992,7 +992,7 @@ THashList*  TStatToolkit::AddMetadata(TTree* tree, const char *varTagName,const 
     - TStatToolkit::MakeGraphErrors
    */
   // 
-  if (!tree) return kFALSE;
+  if (!tree) return NULL;
   THashList * metaData = (THashList*) tree->GetUserInfo()->FindObject("metaTable");
   if (!metaData == 0){  
     metaData=new THashList;