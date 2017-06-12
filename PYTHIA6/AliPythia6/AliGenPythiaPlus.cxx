7734c70482d4710f46e94063d00a2a56bff8b953
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2017-06-12 17:08:22 +0200

    remove class imp/def conditions; simplify return in barrel acceptance

--- PYTHIA6/AliPythia6/AliGenPythiaPlus.cxx
+++ PYTHIA6/AliPythia6/AliGenPythiaPlus.cxx
@@ -35,9 +35,7 @@
 #include "AliLog.h"
 #include "PyquenCommon.h"
 
-/// \cond CLASSIMP
-ClassImp(AliGenPythiaPlus);
-/// \endcond
+ClassImp(AliGenPythiaPlus)
 
 AliGenPythiaPlus::AliGenPythiaPlus():
     AliGenMC(),
@@ -1344,10 +1342,7 @@ void AliGenPythiaPlus::GetSubEventTime()
 ///
 Bool_t AliGenPythiaPlus::IsInBarrel(Float_t eta) const
 {
-  if( eta < fTriggerEta  ) 
-  return kTRUE;
-  else 
-  return kFALSE;
+  return ( eta < fTriggerEta ) ;
 }
 
 ///
@@ -1378,7 +1373,7 @@ Bool_t AliGenPythiaPlus::IsInPHOS(Float_t phi, Float_t eta, Int_t iparticle)
 {
   if(phi > fPHOSMinPhi  && phi < fPHOSMaxPhi && 
      eta < fPHOSEta  ) 
-  return kTRUE;
+    return kTRUE;
   else 
   {
     if( fCheckPHOSeta && eta < fPHOSEta) fPHOSRotateCandidate = iparticle;
