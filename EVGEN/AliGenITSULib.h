6774b2a89e7f713812c5986d1c71426e21517cba
gconesab <26649152+gconesab@users.noreply.github.com>
2020-03-26 16:44:52 +0100

    Port HF and GA developments and fixes from master to v5-09-24 (#1135)
    * Correct use of decayer in AliGenPythiaPlus.
    
    * fPythia object not persistent to avoid default constructur be called when reading galice.root
    
    * Fix index to have the CutOnChild selection working with Pythia8
    
    * Implement kHeavyFlavor as option for SetSTackFillOpt
    
    * Update decay channels and BRs to force charm hadronic decays (#1078)
    
    * Change to SoftQCD the process for HF generation (#1084)
    
    * Order by flavour the partons that are first and second mother of a hadron (#1089)
    
    * debug printf removed
    
    * debug printf removed
    
    * Change PYTHIA8 to 8.243 (#997)
    
    * Omega_ccc added.
    
    * Update AliGenITSULib.cxx
    
    * Fix for newly imported Pythia8
    
    * Adding Xic+->pK-pi+ (including Xic+->pK0*), Xic+->Xi-pi+pi+ (including Xic+->Xi*0 pi+), Xic0->Xi-pi+ decays, OmegaC->OmegaPi to ForceHadronicD; adding Bs->Ds- pi+ to ForceBeautyUpgrade; fixes to ForceBeautyUpgrade in AliDecayerPythia8. Adding Xic0 and Bs to AliGenITSULib. Decay table modified to update Xic0, Xic+ and OmegaC decay channels.
    
    * Add HF selection case with c and b proportions as in Pythia (#1096)
    
    * Overwrite default process (elastic scattering) from decayer initialisation (solves ALIROOT-8407)
    
    * Add option to trigger particles over any of the central barrel calorimeters: PHOS or EMCal or DCal
    
    * Fix lambda_b lifetime in Pythia8
    
    * switch pi0 decay on in pythia8 decayer
    
    Co-authored-by: Andreas Morsch <andreas.morsch@cern.ch>
    Co-authored-by: Francesco Prino <prino@to.infn.it>
    Co-authored-by: ddobrigk <david.dobrigkeit.chinellato@cern.ch>
    Co-authored-by: Giulio Eulisse <giulio.eulisse@cern.ch>
    Co-authored-by: Andrea <andrea.rossicern.ch>

--- EVGEN/AliGenITSULib.h
+++ EVGEN/AliGenITSULib.h
@@ -17,7 +17,7 @@ class AliGenITSULib :public AliGenLib {
 
  public:
 
-  enum EPartId {kLb=5122,kLc=4122,kXi_c = 4232,kBplus = 521,kBzero = 511,kDs=431,kDplus=411};
+  enum EPartId {kLb=5122,kLc=4122,kXi_c = 4232,kBplus = 521,kBzero = 511,kDs=431,kDplus=411, kOmega_ccc=4444,kXi_czero = 4132, kOmega_c=4332, kBs=531};
 
   //Getters
     
@@ -31,17 +31,25 @@ class AliGenITSULib :public AliGenLib {
   static Int_t IpLcMinus(TRandom * /*ran*/) {return    -(int)kLc;}
   static Int_t IpLb(TRandom * /*ran*/)      {return     (int)kLb;}
   static Int_t IpLbBar(TRandom * /*ran*/)   {return    -(int)kLb;}
+  static Int_t IpXicZero(TRandom * /*ran*/)     {return   (int)kXi_czero;}
+  static Int_t IpXicZeroBar(TRandom * /*ran*/)  {return  -(int)kXi_czero;}
   static Int_t IpXic(TRandom * /*ran*/)     {return   (int)kXi_c;}
   static Int_t IpXicBar(TRandom * /*ran*/)  {return  -(int)kXi_c;}
+  static Int_t IpOmegac(TRandom * /*ran*/)     {return   (int)kOmega_c;}
+  static Int_t IpOmegacBar(TRandom * /*ran*/)  {return  -(int)kOmega_c;}
   static Int_t IpBPlus(TRandom * /*ran*/)   {return      (int)kBplus;}
   static Int_t IpBMinus(TRandom * /*ran*/)  {return     -(int)kBplus;}
+  static Int_t IpBs(TRandom * /*ran*/)  {return         (int)kBs;}
+  static Int_t IpBsBar(TRandom * /*ran*/)  {return         -(int)kBs;}
   static Int_t IpB0(TRandom * /*ran*/)  {return         (int)kBzero;}
   static Int_t IpB0Bar(TRandom * /*ran*/)  {return         -(int)kBzero;}
   static Int_t IpDsPlus(TRandom * /*ran*/)  {return     (int)kDs;}
   static Int_t IpDsMinus(TRandom * /*ran*/) {return    -(int)kDs;}
   static Int_t IpDPlus(TRandom * /*ran*/)   {return  (int)kDplus;}
-  static Int_t IpDMinus(TRandom * /*ran*/)  {return -(int)kDplus;}
+  static Int_t IpDMinus(TRandom * /*ran*/)  {return  -(int)kDplus;}
 
+  static Int_t IpOmegaccc(TRandom * /*ran*/);
+  
   static Double_t PtFlat(const Double_t * /*px*/, const Double_t * /*dummy*/) {return 1;}
   static Double_t YFlat (const Double_t * /*py*/, const Double_t * /*dummy*/) {return 1;}
 
